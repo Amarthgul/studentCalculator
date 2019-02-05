d0152995a74435cc552cfdff8ff3951e67752d18
Amarth Gul <amarthgul@gmail.com>
2019-02-05 10:15:18 -0500

    Update main.py

--- Math/Statistic/main.py
+++ Math/Statistic/main.py
@@ -11,6 +11,7 @@ def comba(k, n):
     '''Combination. Select k from n, without order. '''
     return factorial(n) / (factorial(n - k) * factorial(k));
 
+
 def bino(x, n, p):
     '''Binomial porbablity'''
     return comba(x, n) * p ** x * (1 - p) ** (n - x)
@@ -21,7 +22,23 @@ def pois(x, mu, useMathPkg = False):
     return E ** (-mu) * mu ** x / factorial(x)
 
 
+class unifDist():
+    def __init__(self, start = 0, end = 1):
+        self.__start = start;
+        self.__end = end;
+        self.__prob = 1 / (end - start);
+    def probBetween(self, start, end):
+        start = start if (start >= self.__start) else self.__start;
+        end = end if (end <= self.__end) else self.__end;
+        return (end - start) * self.__prob
+    def probAbove(self, num):
+        if num >= self.__end: return 0;
+        else: return (self.__end - num) * self.__prob;
+    def probBelow(self, num):
+        if num <= self.__start: return 0;
+        else: return (num - self.__start) * self.__prob;
 
+        
 def sciConvert(num, powerBase = 10, power = 2, precision = 4):
     '''Convert a number into scientific format. 
 num: number to be converted;
