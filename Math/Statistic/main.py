a3753bceea6bf3489841c432109f9ae8d1ff67e8
Amarth Gul <amarthgul@gmail.com>
2019-02-12 10:23:06 -0500

    Update main.py

--- Math/Statistic/main.py
+++ Math/Statistic/main.py
@@ -1,4 +1,8 @@
+import math
+
 def factorial(input):
+    '''Compute factorial of a non-negtative number'''
+    if input < 0: return 0;
     result = 1;
     for i in reversed(range(input)):
         result *= (i + 1);
@@ -11,7 +15,6 @@ def comba(k, n):
     '''Combination. Select k from n, without order. '''
     return factorial(n) / (factorial(n - k) * factorial(k));
 
-
 def bino(x, n, p):
     '''Binomial porbablity'''
     return comba(x, n) * p ** x * (1 - p) ** (n - x)
@@ -20,7 +23,6 @@ def pois(x, mu, useMathPkg = False):
     if useMathPkg: import math;
     E = 2.718281828459045 if not useMathPkg else math.exp(1);
     return E ** (-mu) * mu ** x / factorial(x)
-
 
 class unifDist():
     def __init__(self, start = 0, end = 1):
@@ -37,19 +39,9 @@ class unifDist():
     def probBelow(self, num):
         if num <= self.__start: return 0;
         else: return (num - self.__start) * self.__prob;
-
-        
-def sciConvert(num, powerBase = 10, power = 2, precision = 4):
-    '''Convert a number into scientific format. 
-num: number to be converted;
-powerBase: the base of power;
-power: just power... generally _powerBase_ to the power of _power_;
-precision: if there's float point, then count number after the float point.
-    '''
-    result = ""
-    base = str(num / (powerBase ** power)) + "0" * precision
-    if base.find("."):
-        base = base[0: base.find(".") + precision + 1]
-    else: base = base[: precision]
-    result += base + " * " + str(powerBase) + " ^ " + str(power)
-    return result
+    def expectation(self):
+        return (self.__end + self.__start) / 2;
+    def variance(self):
+        return (self.__end - self.__start)**2 / 12;
+    def stdev(self):
+        return math.sqrt(self.variance())
